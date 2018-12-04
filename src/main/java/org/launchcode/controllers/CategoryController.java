7c72a6783b9361629723be3686444ad09b125670
to0thake <joshua_e_santa@outlook.com>
2018-12-03 20:37:53 -0500

    committed

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,12 +1,18 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "category")
@@ -26,13 +32,28 @@ public class CategoryController {
         return "category/index";
     }
 
-//    @RequestMapping(value ="add") // /category/add
-//    public String add(){
-//        //render an add form
-//    }
-//
-//    @RequestMapping (value = "add", method = RequestMethod.POST)
-//    public String add (){
-//        // accepts post from request
-//    }
+    @RequestMapping(value ="add") // /category/add
+    public String add(Model templateVariables){
+        //render an add form
+        // 1: add a title
+        templateVariables.addAttribute("title", "Add Category");
+        // 2: add an Empty object for model binding
+        templateVariables.addAttribute("category", new Category());
+        return "category/add";
+    }
+
+    @RequestMapping (value = "add", method = RequestMethod.POST)
+    public String add (
+        Model templateVariables,
+        @ModelAttribute @Valid Category category,
+        Errors errors
+    ){
+        //1: check if the binded model has errors
+        if (errors.hasErrors()) return "category/add";
+        //2: (no errors) save the category object in the db
+        categoryDAO.save(category);
+        //3: redirect back to the category list (index) view
+        return "redirect:";
+        // accepts post from request
+    }
 }
